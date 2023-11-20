8e579bc7365360b475de28fd365e7c4f321acc2c
Igor Krynkin <igor.krynkin@gmail.com>
2023-11-20 16:47:51 +0300

    Oculus-Site commit 8.2

--- Oculus-Site/js/main.js
+++ Oculus-Site/js/main.js
@@ -20,6 +20,6 @@ function open(evt){
     });
 
     tabTarget.classList.add('tabs__btn-item--active');
-    document.querySelector('#${button}').classList.add('tabs__content-item--active');
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
 
 }
